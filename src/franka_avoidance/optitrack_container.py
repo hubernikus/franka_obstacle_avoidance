014530dcdd0f4d24f09906baa61c9a848aa16c08
Lukas Huber <hubernikus@gmail.com>
2022-12-07 16:47:18 +0100

    Update optitrack / pybullet obstacles.

--- src/franka_avoidance/optitrack_container.py
+++ src/franka_avoidance/optitrack_container.py
@@ -1,12 +1,61 @@
 """
 Container which smoothenes position (and rotation) of incoming obstacles.
 """
+import warnings
+
+try:
+    import pybullet as pb
+except ModuleNotFoundError:
+    warnings.warn("PyBullet not imported - no publishing possible.")
+
 import numpy as np
 
 from dynamic_obstacle_avoidance.obstacles import Obstacle
 from dynamic_obstacle_avoidance.container import ObstacleContainer
 
 from franka_avoidance.optitrack_interface import OptitrackInterface
+
+
+def pybullet_publisher(obstacles):
+    self._pb_obstacle_ids = []
+    # Goal Sphere
+    for oo, obs in enumerate(obstacles):
+        if isinstance(obs, "Ellipse"):
+            # pb_obstacle_type = pb.GEOM_SPHERE
+            pb_obstacle_type = pb.GEOM_ELLIPSOID
+        else:
+            raise NotImplementedError("Given obstacle type has not been defined.")
+
+        self._pb_obstacle_ids.append(
+            pb.createVisualShape(
+                shapeType=pb_obstacle_type,
+                halfExtents=obs.axes_length / 2.0,
+                rgbaColor=[0.1, 0.9, 0.1, 0.9],
+                specularColor=[0.4, 0.4, 0],
+            )
+        )
+        pb_ellipse = pb.createMultiBody(
+            baseMass=0,
+            baseInertialFramePosition=[0, 0, 0],
+            baseVisualShapeIndex=self._pb_obstacle_ids,
+            # basePosition=obstacle.position,
+            useMaximalCoordinates=True,
+        )
+
+
+def pybullet_initializer(obstacles):
+    for oo in range(len(obstacles)):
+        pb.resetBasePositionAndOrientation(
+            self._pb_obstacle_ids[pp], obs.position, obs.orintation.as_quat()
+        )
+
+
+def rviz_publisher(obstacle):
+    pass
+
+
+def rviz_initializer(obstacle):
+    pass
 
 
 class OptitrackContainer(ObstacleContainer):
