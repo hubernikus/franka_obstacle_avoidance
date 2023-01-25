b2d96173ee6c9a2d2aacb49949b235075e553942
Lukas Huber <hubernikus@gmail.com>
2023-01-25 20:26:59 +0100

    Fix recurring update matrix.

--- examples/example_cartesian_custom_damping.py
+++ examples/example_cartesian_custom_damping.py
@@ -1,37 +1,30 @@
 #!/usr/bin/env python3
-import copy
-import math
-
 import numpy as np
-import scipy
-from scipy.spatial.transform import Rotation as Rotation
 
 import rclpy
 from rclpy.node import Node
 
-import threading
-
-# LASA Libraries
+# LASA / Control-Libraries
 import state_representation as sr
 from controllers import create_cartesian_controller, CONTROLLER_TYPE
 from dynamical_systems import create_cartesian_ds, DYNAMICAL_SYSTEM_TYPE
 from network_interfaces.control_type import ControlType
 from network_interfaces.zmq.network import CommandMessage
 
+from vartools.linalg import get_orthogonal_basis
+
 # Custom libraries
 from franka_avoidance.robot_interface import RobotZmqInterface as RobotInterface
-
-from vartools.linalg import get_orthogonal_basis
 
 
 class CartesianSpaceController(Node):
     def __init__(
         self,
-        robot,
-        freq: float = 100,
+        robot: RobotInterface,
+        freq: float = 100.0,
         node_name="velocity_controller",
         is_simulation: bool = True,
-    ):
+    ) -> None:
         super().__init__(node_name)
         self.robot = robot
         self.rate = self.create_rate(freq)
@@ -44,6 +37,7 @@ class CartesianSpaceController(Node):
             self.linear_orthogonal_damping = 1.0
             self.angular_stiffness = 0.5
             self.angular_damping = 0.5
+
         else:
             print("Control mode: real")
             self.linear_principle_damping = 50.0
@@ -105,7 +99,7 @@ class CartesianSpaceController(Node):
     #     while rclpy.ok():
     #         self.controller_callback()
 
-    def create_controller_dissipative(self):
+    def create_controller_dissipative(self) -> None:
         """Simple dissipative controller to obtain the desired state.
         The state is assumed to have zero force / torque,
         as this will be transferred further."""
@@ -120,7 +114,7 @@ class CartesianSpaceController(Node):
 
         D = np.diag(
             [
-                self.linear_principle_damping,
+                self.linear_orthogonal_damping,
                 self.linear_orthogonal_damping,
                 self.linear_orthogonal_damping,
                 self.angular_damping,
@@ -130,9 +124,41 @@ class CartesianSpaceController(Node):
         )
 
         self.ctrl_dissipative.set_parameter_value("damping", D, sr.ParameterType.MATRIX)
-        # return ctrl_dissipative.compute_command(
-        #     desired_twist, state.ee_state, state.jacobian
-        # )
+
+    def update_dissipative_controller(self, desired_twist: sr.CartesianTwist) -> None:
+        linear_velocity = desired_twist.get_linear_velocity()
+        if not (lin_norm := LA.norm(linear_velocity)):
+            D = np.diag(
+                [
+                    self.linear_orthogonal_damping,
+                    self.linear_orthogonal_damping,
+                    self.linear_orthogonal_damping,
+                    self.angular_damping,
+                    self.angular_damping,
+                    self.angular_damping,
+                ]
+            )
+            self.ctrl_dissipative.set_parameter_value(
+                "damping", D, sr.ParameterType.MATRIX
+            )
+            return
+
+        D = np.zeros(6)
+
+        # Angular damping
+        D[3:, 3:] = np.eye(3) * self.angular_damping
+
+        # Linear damping
+        D_linear = np.diag(
+            [
+                self.linear_principle_damping,
+                self.linear_orthogonal_damping,
+                self.linear_orthogonal_damping,
+            ]
+        )
+        E = get_orthogonal_basis(linear_velocity / lin_norm)
+        D[:3, :3] = E @ D_linear @ E.T
+        self.ctrl_dissipative.set_parameter_value("damping", D, sr.ParameterType.MATRIX)
 
     def controller_callback(self) -> None:
         command = CommandMessage()
@@ -140,15 +166,17 @@ class CartesianSpaceController(Node):
 
         state = self.robot.get_state()
         if not state:
-            # print("Waiting for a state.")
             return
+        # !!!Current force / torque are eliminated in order to not 'overcompensate'
         state.ee_state.set_force(np.zeros(3))
         state.ee_state.set_torque(np.zeros(3))
 
         desired_twist = sr.CartesianTwist(self.ds.evaluate(state.ee_state))
         desired_twist.clamp(self.clamp_linear, self.clamp_angular)
 
-        # !!!Current force / torque are eliminated in order to not 'overcompensate'
+        # Update Damping-matrix based on desired velocity
+        self.update_dissipative_controller(desired_twist)
+
         cmnd_dissipative = ctrl_dissipative.compute_command(
             desired_twist, state.ee_state, state.jacobian
         )
@@ -159,8 +187,8 @@ class CartesianSpaceController(Node):
         self.robot.send_command(command)
 
 
-if __name__ == "__main__":
-    print("Starting Cartesian Damping controller  ...")
+if (__name__) == "__main__":
+    print("[INFO] Starting Cartesian Damping controller  ...")
     rclpy.init()
     robot_interface = RobotInterface("*:1601", "*:1602")
 
